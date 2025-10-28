bbcba43df43e33cc51162461ed44c8320bb4a3af
HuJun017 <166590848+HuJun017@users.noreply.github.com>
2025-10-28 10:30:46 +0000

    funzioni aggiungi nuovo studente

--- progetto/src/app/pagina2/pagina2.ts
+++ progetto/src/app/pagina2/pagina2.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';    
+import { CommonModule } from '@angular/common';
+import { StudentiService } from '../services/studenti-service';
 
 @Component({
   selector: 'app-pagina2',
-  imports: [],
-  templateUrl: './pagina2.html',
-  styleUrl: './pagina2.css',
+  standalone: true,                            
+  imports: [CommonModule, FormsModule],          
+  templateUrl: './pagina2.html'
 })
 export class Pagina2 {
+  studenti: any[] = [];
+  nome = '';
+  classe = '';
+  mediaVoti: number | null = null;
 
+  constructor(private studentiService: StudentiService) {}
+
+  ngOnInit() {
+    this.studenti = this.studentiService.getStudenti();
+  }
+
+  aggiungiStudente() {
+    const nuovoStudente = {
+      nome: this.nome,
+      classe: this.classe,
+      mediaVoti: this.mediaVoti
+    };
+
+    this.studentiService.addStudente(nuovoStudente);
+    this.studenti = this.studentiService.getStudenti();
+
+    // pulisci i campi
+    this.nome = '';
+    this.classe = '';
+    this.mediaVoti = null;
+  }
 }
